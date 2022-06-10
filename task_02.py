77655c4fa1c857b83d9b83128f6b8d0a61803d64
Last_Vegas <AC-130a@yandex.ru>
2022-06-10 15:03:40 +0300

    Create method for showing random square matrix

--- task_02.py
+++ task_02.py
@@ -2,3 +2,11 @@
 # квадратичную матрицу с количеством строк и столбцов равному
 # этому числу, программа заполняет матрицу случайными числами
 # от 10 до 99
+
+from random import randint
+number = int(input("Number "))
+for g in range(number):
+    part = []
+    for h in range(number):
+        part.append(randint(10, 99))
+    print(part)
