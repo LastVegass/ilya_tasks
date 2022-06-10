af809905b5c725203ffce4650267ef952ec01fed
Last_Vegas <AC-130a@yandex.ru>
2022-06-10 14:35:48 +0300

    Create Algorithm for Finding simple numbers multiple to 13

--- task_01.py
+++ task_01.py
@@ -1,6 +1,7 @@
 # программа на диапазоне чисел от 15000000 до 25000000
 # находит все простые числа сумма цифр которых кратна 13
-numbers = []
+simple = []
+odd13 = []
 for s in range(15000000, 25000000):
     x = 0
     for j in range(s):
@@ -8,6 +9,17 @@ for s in range(15000000, 25000000):
             x = x+1
         if x>=3:
             break
-    if x==2:
-        numbers.append(s)
-print(numbers)
+    if x == 2:
+        simple.append(s)
+print(simple)
+for i in simple:
+    g = 0
+    f = i
+    for y in range(i):
+        g = g+f%10
+        f = f//10
+        if f<0:
+            break
+    if g%13==0:
+        odd13.append(i)
+print(odd13)
