7789eaadbced661f7b51e428475ddd4c98d29b73
Last_Vegas <AC-130a@yandex.ru>
2022-06-22 17:48:54 +0300

    Homework 22.06.22

--- task_02.py
+++ task_02.py
@@ -2,9 +2,8 @@
 # квадратичную матрицу с количеством строк и столбцов равному
 # этому числу, программа заполняет матрицу случайными числами
 # от 10 до 99
-from random import randint
-
-number = int(input("Number "))
+# number = int(input("Number "))
+number = 4
 arr = []
 for g in range(number):
     part = []
@@ -16,28 +15,52 @@ for g in range(number):
 print('#####')
 newarr = []
 for i in arr:
-    print(i)
+   print(i)
 indver = 0
 indhor = 0
 for g in arr:
     for i in g:
         if indver == 0 and indhor == 0:
-            newarr.append(arr[indver][indhor+1]+arr[indver+1][indhor+1]+arr[indver+1][indhor+1])
+            newarr.append(arr[indver][indhor+1]
+                          +arr[indver+1][indhor+1]
+                          +arr[indver+1][indhor+1]
+                         )
             indhor = indhor + 1
         elif indver == 0 and indhor == number-1:
-            newarr.append(arr[indver][indhor-1]+arr[indver+1][indhor]+arr[indver+1][indhor-1])
+            newarr.append(arr[indver][indhor-1]
+                          +arr[indver+1][indhor]
+                          +arr[indver+1][indhor-1]
+                         )
         elif indver == 0 and indhor>0:
-            newarr.append(arr[indver][indhor-1]+arr[indver+1][indhor-1]+arr[indver+1][indhor+1]+arr[indver+1][indhor+1]+arr[indver+0][indhor+1])
+            newarr.append(arr[indver][indhor-1]
+                          +arr[indver+1][indhor-1]
+                          +arr[indver+1][indhor+1]
+                          +arr[indver+1][indhor+1]
+                          +arr[indver+0][indhor+1]
+                         )
             indhor = indhor +1
         elif indver > 0 and indhor == 0:
-            newarr.append(arr[indver-1][indhor]+arr[indver-1][indhor+1]+arr[indver][indhor+1]+arr[indver+1][indhor+1]+arr[indver+1][indhor])
+            newarr.append(arr[indver-1][indhor]
+                          +arr[indver-1][indhor+1]
+                          +arr[indver][indhor+1]
+                          +arr[indver][indhor+1]
+                          +arr[indver][indhor]
+                         )
             indhor = indhor +1
-        elif indver>0 and indhor>0:
-            newarr.append(arr[indver-1][indhor]+arr[indver-1][indhor+1]+arr[indver][indhor+1]+arr[indver+1][indhor+1]+arr[indver+1][indhor]+arr[indver+1][indhor-1]+arr[indver][indhor-1]+arr[indver-1][indhor-1])
-            indhor = indhor +1
+        elif indver > 0 and indhor > 0:
+            newarr.append(arr[indver-1][indhor]
+                          +arr[indver-1][indhor+1]
+                          +arr[indver][indhor+1]
+                          +arr[indver][indhor+1]
+                          +arr[indver][indhor]
+                          +arr[indver][indhor-1]
+                          +arr[indver][indhor-1]
+                          +arr[indver-1][indhor-1]
+                         )
+            # indver = indver +1
     indhor = 0
     indver = indver + 1
     print(newarr)
-    
+
 for j in newarr:
     print(j)
