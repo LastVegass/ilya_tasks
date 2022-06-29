3c1bf3ee072f7a1e4b2c8a6ef49df8cd63539d56
Last_Vegas <AC-130a@yandex.ru>
2022-06-29 19:31:48 +0300

    Added new functions

--- task_03.py
+++ task_03.py
@@ -3,24 +3,36 @@
 # маркой, стоимостью в долларах США, годом выпуска и
 # страной производства, при создании объекта класса потребуется
 # только марка машины, далее пользователь самостоятельно вводит
-# всю ирформацию через функцию
-
+# # всю ирформацию через функцию
+#
 class Car():
-
     def __init__(self, brand):
-        self.brand = brand
-        self.features = {'mass': [],
-                         'power': [],
-                         'cost': [],
-                         'year': [],
-                         'nation': []
-                        }
+            self.brand = brand
+            self.features = {'weight': [],
+                             'power': [],
+                             'cost': [],
+                             'year': [],
+                             'country': []
+                            }
 
-    def carInfo(self):
-        for i in range(5):
-            self.features.append(input())
+    def carInfo(self, weight, power, cost, year, nation):
+        for k,v in self.features.items():
+            self.features[k].append(input('Please input '))
+
+    def changeFtr(self):
+        feature = input('Which feature to change: ')
+        if feature!='weight' or feature!='power' or feature!='cost' or feature!='year' or feature!='country':
+            print('ERROR: Given feature does not exist')
+        else:
+            changedvalue = input('New value: ')
+            self.features[feature].clear()
+            self.features[feature].append(changedvalue)
 
 x = Car("toyota")
 print(x.brand,x.features)
-x.carInfo()
-print(x.brand,x.features)
+x.carInfo(100, 100, 10000, 1997, 'Sweden')
+print(x.features)
+print(type(x.features['weight']))
+x.changeFtr()
+print(x.features['weight'])
+print(x.features)
