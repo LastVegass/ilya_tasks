5e5ef6e3a6ba7bd806c0e6fe6958431a2a15a790
Last_Vegas <AC-130a@yandex.ru>
2022-07-06 09:15:04 +0300

    Finished the task

--- task_03.py
+++ task_03.py
@@ -1,10 +1,3 @@
-# Программа должна создавать объект класса машина
-# машина будет обладать массой, мощностью двигателя в л.с.,
-# маркой, стоимостью в долларах США, годом выпуска и
-# страной производства, при создании объекта класса потребуется
-# только марка машины, далее пользователь самостоятельно вводит
-# # всю ирформацию через функцию
-#
 class Car():
     def __init__(self, brand):
             self.brand = brand
@@ -15,24 +8,20 @@ class Car():
                              'country': []
                             }
 
-    def carInfo(self, weight, power, cost, year, nation):
-        for k,v in self.features.items():
-            self.features[k].append(input('Please input '))
+    def carInfo(self, weight, power, cost, year, country):
+        self.features['weight'].append(weight)
+        self.features['power'].append(power)
+        self.features['cost'].append(cost)
+        self.features['year'].append(year)
+        self.features['country'].append(country)
 
-    def changeFtr(self):
-        feature = input('Which feature to change: ')
-        if feature!='weight' or feature!='power' or feature!='cost' or feature!='year' or feature!='country':
-            print('ERROR: Given feature does not exist')
-        else:
-            changedvalue = input('New value: ')
+    def changeFtr(self, feature, changedvalue):
             self.features[feature].clear()
             self.features[feature].append(changedvalue)
 
 x = Car("toyota")
-print(x.brand,x.features)
 x.carInfo(100, 100, 10000, 1997, 'Sweden')
 print(x.features)
-print(type(x.features['weight']))
-x.changeFtr()
-print(x.features['weight'])
+x.changeFtr('weight', 34)
+x.changeFtr('year', 2000)
 print(x.features)
